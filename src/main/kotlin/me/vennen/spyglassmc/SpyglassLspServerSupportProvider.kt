fddc2b0a06d3525f7fe78a719e95b6dfffbd72ee
Julian Vennen <julian@vennen.me>
2025-02-04 20:54:49 +0100

    Remove companion object

--- src/main/kotlin/me/vennen/spyglassmc/SpyglassLspServerSupportProvider.kt
+++ src/main/kotlin/me/vennen/spyglassmc/SpyglassLspServerSupportProvider.kt
@@ -2,30 +2,25 @@
 
 package me.vennen.spyglassmc
 
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
-import com.intellij.execution.configurations.GeneralCommandLine
-import me.vennen.spyglassmc.SpyglassLspServerSupportProvider.Companion.supportedFileTypes
 
 class SpyglassLspServerSupportProvider : LspServerSupportProvider {
-    companion object {
-        val supportedFileTypes = arrayOf("mcfunction", "json")
-    }
-
     override fun fileOpened(
         project: Project,
         file: VirtualFile,
         serverStarter: LspServerSupportProvider.LspServerStarter
     ) {
-        if (supportedFileTypes.contains(file.extension)) {
+        if (arrayOf("mcfunction", "json").contains(file.extension)) {
             serverStarter.ensureServerStarted(SpyglassLspServerDescriptor(project))
         }
     }
 }
 
 private class SpyglassLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "SpyglassMC") {
-    override fun isSupportedFile(file: VirtualFile) = supportedFileTypes.contains(file.extension)
-    override fun createCommandLine() = GeneralCommandLine("npx", "@spyglassmc/language-server", "--stdio")
+    override fun isSupportedFile(file: VirtualFile) = arrayOf("mcfunction", "json").contains(file.extension)
+    override fun createCommandLine() = GeneralCommandLine("node", "--inspect-brk=9229", "/home/julian/workspace/Spyglass/packages/language-server/bin/server.js", "--stdio")
 }
